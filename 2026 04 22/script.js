701ad8e26655d0a2cdea0552c11b8a963cbeb468
MeatGamer <justas159321@gmail.com>
2026-04-22 13:20:36 +0300

    Add files via upload

--- 2026 04 22/script.js
+++ 2026 04 22/script.js
@@ -1,37 +1,44 @@
 // async function getPost() {
-//     let response = await fetch ('https://jsonplaceholder.typicode.com/todos/1')
+//     let response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
 //     response = await response.json()
-//     console.log(response)
-//     console.log(response.userId)
+//     console.log(response);
 //     console.log(response.title)
 // }
 
-async function getPost() {
-    let response = await fetch ('https://jsonplaceholder.typicode.com/posts')
-    response = await response.json()
-    return response
+async function getPosts() {
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    response = await response.json();
+    return response;
+}
+async function getUsers() {
+    let response = await fetch('https://jsonplaceholder.typicode.com/users')
+    response = await response.json();
+    return response;
+}
+
+function getUserByID(users, id) {
+    return users.find(user => user.id == id);
 }
 
 window.onload = async () => {
-    const posts = await getPost()
-    console.log(posts)
-    // for(let i = 0; i < posts.length; i++){
-    //     const post = document.getElementById('post')
-    //     const li = document.createElement('li')
-    //     li.textContent = posts[i].title
-    //     post.appendChild(li)
-    // }
+    const posts = await getPosts();
+    const users = await getUsers();
 
-    const postElements = document.getElementById('post')
+    console.log(posts);
+
+    const postsElement = document.getElementById("posts");
 
     posts.forEach(post => {
-        const postElement = document.createElement('div')
+        const postElement = document.createElement('div');
+        postElement.classList.add('post');
+
+        const user = getUserByID(users, post.userId);
+
         postElement.innerHTML = `
             <p>${post.title}</p>
-            <p>${post.body}</p>        
-        `
-        postElements.append(postElement)
+            <p>${post.body}</p>
+            <p>${user.name}</p>
+        `;
+        postsElement.append(postElement);
     });
 }
-
-
